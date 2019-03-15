a46168b931b00270e69886e94129b41bd6750da0
wesleyrocha90 <wesleyallanrocha@gmail.com>
2019-03-15 00:35:30 -0300

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 0.08f;
+    public Rigidbody2D rb;
+
+    private string tagRight = "GoalRight";
+    private string tagLeft = "GoalLeft";
+
+    void Start()
+    {
+        string tag = Random.Range(0, 1) == 0 ? tagRight : tagLeft;
+        StartCoroutine(Launch(tag));
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        StopAllCoroutines();
+        if (collider.tag == tagRight)
+        {
+            StartCoroutine(Launch(collider.tag));
+        }
+        if (collider.tag == tagLeft)
+        {
+            StartCoroutine(Launch(collider.tag));
+        }
+    }
+
+    IEnumerator Launch(string tag)
+    {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(-70f, 70f));
+        Vector3 force = (tag == tagRight ? transform.right : -transform.right) * speed;
+        yield return new WaitForSeconds(2);
+        rb.AddForce(force);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private float maxHeight = 3.8f;
+    private float minHeight = -3.8f;
+    private float verticalMovement;
+    
+    void Update()
+    {
+        verticalMovement = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if ((verticalMovement > 0 && transform.position.y < maxHeight)
+            || (verticalMovement < 0 && transform.position.y > minHeight))
+        {
+            transform.Translate(Vector3.up * verticalMovement * speed * Time.deltaTime);
+        }
+    }
+}
